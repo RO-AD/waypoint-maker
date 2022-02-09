f24661316b56138dd274810548398d21ee350fc6
hyojinlee98 <dnfmfmzhkd12@naver.com>
2022-02-10 01:37:26 +0900

    재승이 수정사항과 병합 & 마커 이미지 변경 & 마커 여러 개 표시 & ctrl 키 입력 유무 확인 & 마커 삭제 미완

--- js/map.js
+++ js/map.js
@@ -27,7 +27,12 @@ function utm_to_longlat(x, y) {
 $(function() {
     //var $div = create_map_element();
     
+    // ctrl 입력 유무
+    let is_ctrl_press = false;
+
     var container = $('.map-container')[0];
+    // KCITY : (37.22932411777117, 126.77329790671789)
+    // KGU : (37.30061299648025, 127.03577935414826)
     var options = {
         center: new kakao.maps.LatLng(37.30061299648025, 127.03577935414826),
         level: 3
@@ -35,20 +40,44 @@ $(function() {
 
     var map = new kakao.maps.Map(container, options);
 
-    var marker = new kakao.maps.Marker({
-        // 지도 중심좌표에 마커를 생성합니다
-        position: map.getCenter()
-    });
+    // 마커 이미지
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+    var imageSize = new kakao.maps.Size(24, 35); 
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+    
+    var positions = []
+    var markers = []
 
-    // 지도에 마커를 표시합니다
-    marker.setMap(map);
+    document.addEventListener('keydown', function(key_event) {
+        if (key_event.ctrlKey) {
+            is_ctrl_press = true;
+            //console.log('down ' + is_ctrl_press);
+        }
+    })
+    
+    document.addEventListener('keyup', function() {
+        is_ctrl_press = false;
+        //console.log('up ' + is_ctrl_press);
+    })
 
     // 지도에 클릭 이벤트를 등록합니다
     // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+    kakao.maps.event.addListener(map, 'click', function(mouse_event) {
+        var marker = new kakao.maps.Marker({
+            position: mouse_event.latLng, // 마커 생성 위치
+            image : markerImage // 마커 이미지 
+        });
 
-    // 클릭한 위도, 경도 정보를 가져옵니다
-        var latlng = mouseEvent.latLng;
+        if (is_ctrl_press) {
+            marker.setMap(null);
+            return true;
+        }
+        marker.setMap(map);
+        // console.log(mouse_event.latLng.La, mouse_event.latLng.Ma);
+
+
+        // 클릭한 위도, 경도 정보를 가져옵니다
+        var latlng = mouse_event.latLng;
 
         // 마커 위치를 클릭한 위치로 옮깁니다
         marker.setPosition(latlng);
@@ -56,13 +85,8 @@ $(function() {
         var lat = latlng.getLat();
         var lng = latlng.getLng();
 
-        var message = '위도 : ' + lat;
-        message += ' / 경도 : ' + lng;
+        console.log('위도 : ' + lat + ' / 경도 : ' + lng);
 
-        var resultDiv = document.getElementById('map');
-        //resultDiv.innerHTML = message;
-        console.log(message);
-        console.log(longlat_to_utm(lng, lat))
 
     });
 });
