a946a4a519a51369307c6cf710aab96f61c2911b
hyojinlee98 <dnfmfmzhkd12@naver.com>
2022-02-13 00:28:18 +0900

    마커 클릭 시 삭제 추가(키보드 이벤트 및 is_ctrl_press 변수 삭제)

--- js/map.js
+++ js/map.js
@@ -23,12 +23,10 @@ function utm_to_longlat(x, y) {
     return projected_result;
 }
 
+console.log(longlat_to_utm(127.03257673041796, 37.296834146070275));
 
 $(function() {
     //var $div = create_map_element();
-    
-    // ctrl 입력 유무
-    let is_ctrl_press = false;
 
     var container = $('.map-container')[0];
     // KCITY : (37.22932411777117, 126.77329790671789)
@@ -46,32 +44,23 @@ $(function() {
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
     
     var positions = []
-    var markers = []
 
-    document.addEventListener('keydown', function(key_event) {
-        if (key_event.ctrlKey) {
-            is_ctrl_press = true;
-            //console.log('down ' + is_ctrl_press);
-        }
-    })
-    
-    document.addEventListener('keyup', function() {
-        is_ctrl_press = false;
-        //console.log('up ' + is_ctrl_press);
-    })
 
-    // 지도에 클릭 이벤트를 등록합니다
-    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+    // 지도 클릭 이벤트
     kakao.maps.event.addListener(map, 'click', function(mouse_event) {
         var marker = new kakao.maps.Marker({
             position: mouse_event.latLng, // 마커 생성 위치
             image : markerImage // 마커 이미지 
         });
 
-        if (is_ctrl_press) {
+        // 마커 클릭 이벤트 (마커 삭제)
+        kakao.maps.event.addListener(marker, 'click', function() {
             marker.setMap(null);
+            console.log('삭제');
+
             return true;
-        }
+        });
+
         marker.setMap(map);
         // console.log(mouse_event.latLng.La, mouse_event.latLng.Ma);
 
@@ -86,7 +75,9 @@ $(function() {
         var lng = latlng.getLng();
 
         console.log('위도 : ' + lat + ' / 경도 : ' + lng);
-
+        var utm_position = longlat_to_utm(lng, lat);
+        console.log(utm_position[0] - 325618.68229865003, utm_position[1] - 4129608.2187828263); // UTM
+        console.log(utm_position[0] - 4129608.2187828263, utm_position[1] -325618.68229865003 ); // UTM
 
     });
 });
